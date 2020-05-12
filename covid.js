73a4fa49d98757a191e5d2124bfe7e5ae6e5664b
Zlatex <maximzlatogorsky@gmail.com>
2020-05-12 22:35:27 +0300

    Добавлены коментарии

--- covid.js
+++ covid.js
@@ -3,20 +3,20 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 export async function Start (callback,country){
-    await fetch('https://corona.lmao.ninja/v2/countries/804?yesterday=false&strict&query%20')
+    await fetch(`https://corona.lmao.ninja/v2/countries/${country}?yesterday=false&strict&query%20`)
     .then(res => res.text())
-    .then(body => CovidInfo = JSON.parse(body));
-    checkAndPost(callback);  
+    .then(body => CovidInfo = JSON.parse(body)); // Присваивает переменной CovidInfo актуальные данны об коронавирусе
+    checkAndPost(callback);  //Проверить изменились ли данные с последней рассылки
 }
 
 export async function checkAndPost (callback){
-    if (!CovidInfo) return;
-    var status = fs.readFileSync('./status.json');
+    if (!CovidInfo) return;//Если CovidInfo = undefined, то выходим
+    var status = fs.readFileSync('./status.json');// Информация об последней рассылке
     status = JSON.parse(status);
-    if (status.updated !== CovidInfo.updated) {
-        callback(CovidInfo.updated,CovidInfo.cases,CovidInfo.todayCases,
-            CovidInfo.deaths,CovidInfo.todayDeaths,CovidInfo.recovered,
-            CovidInfo.active,CovidInfo.critical,CovidInfo.tests);
-        fs.writeFileSync('./status.json', JSON.stringify(CovidInfo));
-    }
+    if (status.updated === CovidInfo.updated) return; // Если данные совпадают, то выходим
+    callback(CovidInfo.updated,CovidInfo.cases,CovidInfo.todayCases, //
+        CovidInfo.deaths,CovidInfo.todayDeaths,CovidInfo.recovered, // возвращает в main.js информацию об коронавирусе, чтобы отправить её пользователям
+        CovidInfo.active,CovidInfo.critical,CovidInfo.tests);      // 
+
+    fs.writeFileSync('./status.json', JSON.stringify(CovidInfo));//Сохранение актуальной информации
 }
--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-import * as botInfo from './botInfo.js'
+import * as botInfo from './botInfo.js' //Статичная информация для дальнейшего использования ботом
 import Telegraf  from 'telegraf';
 import * as covid from './covid.js'
 import fs from 'fs';
 
-const token = botInfo.token
+const bot = new Telegraf(botInfo.token); 
 
-const bot = new Telegraf(token);
-//https://api.covid19api.com/summary
-const dtf = new Intl.DateTimeFormat('ua', { year: '2-digit', month: '2-digit', day: '2-digit' }) 
-var postFun = async function (date=null,Confirmed,newConfirmed,Deaths,newDeaths,Recovered,active,critical,tests){
-    if (date === null) return;
-    var users = fs.readFileSync('./chats.json');
+const dtf = new Intl.DateTimeFormat('ua', { year: '2-digit', month: '2-digit', day: '2-digit' }) //на StackOverwlow нашел, переводит Date в месяци, дни, годы
+
+var postFun = async function (date=null,Confirmed,newConfirmed,Deaths,newDeaths,Recovered,active,critical,tests){//Функция, которая отправляет людям из списка сообщение об ситуации с коронавирусом
+    if (date === null) return; // если ничего не приходит (всё переменные равны undefined)
+    var users = fs.readFileSync('./chats.json');//Список пользователей, которым нужно отправить рассылку
     users = JSON.parse(users)[0];
-    const NowDATE = new Date();
-    const leftDays = parseInt((botInfo.quarantineEndDate-NowDATE) / 60000 / 60 / 24)
-    const [{ value: m },,{ value: d },,{ value: y }] = dtf.formatToParts(botInfo.quarantineEndDate); 
-    users.forEach(async (id,index)=>{
-        if (index % 10 === 0) await sleep(1000)
+    const NowDATE = new Date();// Текущее время
+    const leftDays = parseInt((botInfo.quarantineEndDate-NowDATE) / 60000 / 60 / 24)// Осталось дней до конца карантина
+    const [{ value: m },,{ value: d },,{ value: y }] = dtf.formatToParts(botInfo.quarantineEndDate); //на StackOverwlow нашел, переводит Date в месяци, дни, годы
+    users.forEach(async (id,index)=>{ //Думаю тут всё понятно, масив, который перебирает пользователей, которым нужно отправить рассылку
+        if (index % 10 === 0) await sleep(1000)//Каждый 10 пользователей делать паузу так как в телеграме лимиты 30 в секунду(10 чтобы сильно не нагружать бота)
         const [{ value: mo },,{ value: da },,{ value: ye }] = dtf.formatToParts(new Date(date)); 
 
         bot.telegram.sendMessage(id,`
         Коронавірус в Україні станом на ${mo}.${da}.${ye}\n\nВсього захворіло: ${Confirmed}(${newConfirmed} нових)\
         \nВсього хворих: ${active}(${critical} в критичному стані)\nПомерло: ${Deaths}(${newDeaths} нових)\
         \nВиліковано: ${Recovered}\nВсього протестовано: ${tests}\n\nКарантин до ${m}.${d}.${y}(${leftDays} днів осталось)
-        `)
+        `)//Отправка сообщения
     })
 }
 covid.Start(postFun,botInfo.Country);
 
-setInterval(()=>covid.checkAndPost(postFun),60000)
+setInterval(()=>covid.checkAndPost(postFun),60000)//Проверяет каждую минуту не появились ли новые данные
 
 
 var sleep = (ms) => {return new Promise(resolve => setTimeout(resolve, ms));}
